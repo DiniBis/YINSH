09c021be7bd5be0eecc9ff4fe73262189fa0e580
ASAPLIX <118887966+ASAPLIX@users.noreply.github.com>
2024-06-02 23:43:33 +0200

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,3 +1,5 @@
+import pygame.mixer
+
 from back import * #import de toutes les fonctions en back
 from front import * #import de toutes les fonctions d'affichage
 from MainMenu import *#import des fonctions du MainMenu différent pour éviter les erreurs
@@ -14,7 +16,6 @@ def boucle_de_jeu(blitz):
     #chaque joueur place ses 5 anneaux
     tourJoueur=1
     afficher_plateau(plateau._plateau, tourJoueur, 1)
-    pygame.mixer.music.play(-1)
     for i in range(10):
         x,y = clic()
         est_place=plateau.placerAnneau(tourJoueur,x,y)
@@ -41,13 +42,18 @@ def boucle_de_jeu(blitz):
         while res==False:
             nouv_x, nouv_y = clic()
             res=plateau.deplacerAnneau(tourJoueur,liste,x,y,nouv_x,nouv_y)
-        print(plateau._plateau[x][y].getJoueur())
         #Regarder s'il y a un alignement
         tableau_alignement=plateau.alignement()
         #Si des alignements sont enregistrés
         if len(tableau_alignement)>0:
-            res=plateau.retirer_alignement(tableau_alignement, x, y, False)
             afficher_plateau(plateau._plateau, tourJoueur, 3)
+            x, y = clic()
+            if [x,y] in tableau_alignement:
+                res=plateau.retirer_alignement(tableau_alignement, x, y, False)
+            else:
+                while [x,y] not in tableau_alignement:
+                    x, y = clic()
+                    res = plateau.retirer_alignement(tableau_alignement, x, y, False)
             #res est le numéro du joueur qui doit retirer son anneau
             if res==1:
                 #prendre un input de joueur et si c'est l'un de ses anneaux le retirer:
@@ -55,12 +61,26 @@ def boucle_de_jeu(blitz):
                 if isinstance(plateau._plateau[x][y],Anneau) and plateau._plateau[x][y].getJoueur()==1:
                     plateau._plateau[x][y]=0
                     plateau.anneau_retiréJ1+=1
+                    pygame.mixer.Sound.play(align_sound)
+                else:
+                    while not (isinstance(plateau._plateau[x][y],Anneau) and plateau._plateau[x][y].getJoueur()==1):
+                        x, y = clic()
+                        plateau._plateau[x][y] = 0
+                        plateau.anneau_retiréJ1 += 1
+                        pygame.mixer.Sound.play(align_sound)
             elif res==2:
                 #idem pour J2
                 x, y = clic()
                 if isinstance(plateau._plateau[x][y],Anneau) and plateau._plateau[x][y].getJoueur()==2:
                     plateau._plateau[x][y]=0
                     plateau.anneau_retiréJ2+=1
+                    pygame.mixer.Sound.play(align_sound)
+                else:
+                    while not (isinstance(plateau._plateau[x][y],Anneau) and plateau._plateau[x][y].getJoueur()==2):
+                        x, y = clic()
+                        plateau._plateau[x][y] = 0
+                        plateau.anneau_retiréJ1 += 1
+                        pygame.mixer.Sound.play(align_sound)
         tourJoueur=1 if tourJoueur==2 else 2
     return plateau.anneau_retiréJ1, plateau.anneau_retiréJ2 #si la condition de victoire est remplie, envoyer les resultat pour l'écran de fin
 
@@ -113,6 +133,13 @@ def versus_cpu(blitz):
                 if isinstance(plateau._plateau[x][y],Anneau) and plateau._plateau[x][y].getJoueur()==1:
                     plateau._plateau[x][y]=0
                     plateau.anneau_retiréJ1+=1
+                    pygame.mixer.Sound.play(align_sound)
+                else:
+                    while not (isinstance(plateau._plateau[x][y],Anneau) and plateau._plateau[x][y].getJoueur()==1):
+                        x, y = clic()
+                        plateau._plateau[x][y] = 0
+                        plateau.anneau_retiréJ1 += 1
+                        pygame.mixer.Sound.play(align_sound)
             #pas d'opérations pour le CPU ici, les actions sont déjà réalisées dans la fonction
         tourJoueur=1 if tourJoueur==2 else 2
     return plateau.anneau_retiréJ1, plateau.anneau_retiréJ2 #si la condition de victoire est remplie, envoyer les resultat pour l'écran de fin
