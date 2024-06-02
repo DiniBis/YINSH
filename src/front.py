b5a0b159ad2bff7428e01e965f03641dc3a004c9
ASAPLIX <118887966+ASAPLIX@users.noreply.github.com>
2024-06-02 20:54:43 +0200

    Update front.py

--- src/front.py
+++ src/front.py
@@ -13,7 +13,7 @@ black = [0, 0, 0]
 window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption('Yinsh 2049')
 
-plateau_image = pygame.image.load('Board.svg')
+plateau_image = pygame.image.load('board.svg')
 plateau_width, plateau_height = plateau_image.get_size()
 plateau_x = (WINDOW_WIDTH - plateau_width) // 2
 plateau_y = (WINDOW_HEIGHT - plateau_height) // 2
