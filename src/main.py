2285b3a228e676f9bb5e8bac262bf9f31d72fc2d
ASAPLIX <118887966+ASAPLIX@users.noreply.github.com>
2024-06-02 22:52:30 +0200

    Update main.py

--- src/main.py
+++ src/main.py
@@ -14,6 +14,7 @@ def boucle_de_jeu(blitz):
     #chaque joueur place ses 5 anneaux
     tourJoueur=1
     afficher_plateau(plateau._plateau, tourJoueur, 1)
+    pygame.mixer.music.play(-1)
     for i in range(10):
         x,y = clic()
         est_place=plateau.placerAnneau(tourJoueur,x,y)
@@ -45,17 +46,16 @@ def boucle_de_jeu(blitz):
         tableau_alignement=plateau.alignement()
         #Si des alignements sont enregistrés
         if len(tableau_alignement)>0:
-            res=Grille.retirer_alignement(tableau_alignement, x, y, False)
+            res=plateau.retirer_alignement(tableau_alignement, x, y, False)
+            afficher_plateau(plateau._plateau, tourJoueur, 3)
             #res est le numéro du joueur qui doit retirer son anneau
             if res==1:
-                afficher_plateau(plateau._plateau, tourJoueur, 3)
                 #prendre un input de joueur et si c'est l'un de ses anneaux le retirer:
                 x, y = clic()
                 if isinstance(plateau._plateau[x][y],Anneau) and plateau._plateau[x][y].getJoueur()==1:
                     plateau._plateau[x][y]=0
                     plateau.anneau_retiréJ1+=1
             elif res==2:
-                afficher_plateau(plateau._plateau, tourJoueur, 3)
                 #idem pour J2
                 x, y = clic()
                 if isinstance(plateau._plateau[x][y],Anneau) and plateau._plateau[x][y].getJoueur()==2:
@@ -70,6 +70,7 @@ def versus_cpu(blitz):
     plateau=Grille()
     #chaque joueur place ses 5 anneaux
     tourJoueur=1
+    afficher_plateau(plateau._plateau, tourJoueur, 1)
     for i in range(10):
         #entrée des coordonnées par le joueur ou le bot
         x, y = clic() if tourJoueur == 1 else coordonnee_alea(len(plateau._plateau), len(plateau._plateau[0]))
@@ -77,15 +78,16 @@ def versus_cpu(blitz):
         while est_place==False:
             #Tant que le pion n'est pas placé, demander à nouveau
             if tourJoueur==1:
-                x=0 #prendre l'input x du joueur
-                y=0 #prendre l'input y du joueur
+                x, y = clic() #prendre l'input y du joueur
             else:
                 x, y = coordonnee_alea(len(plateau._plateau),len(plateau._plateau[0]))
             est_place=plateau.placerAnneau(tourJoueur,x,y)
         tourJoueur=1 if tourJoueur==2 else 2
+        afficher_plateau(plateau._plateau, tourJoueur, 1)
     #boucle principale
     while victoire(plateau.anneau_retiréJ1, plateau.anneau_retiréJ2, blitz)==False:
         #entrée des coordonnées par le joueur ou le bot
+        afficher_plateau(plateau._plateau, tourJoueur, 2)
         x, y = clic() if tourJoueur == 1 else coordonnee_alea(len(plateau._plateau), len(plateau._plateau[0]))
         #Le joueur du tour choisis un anneau qui sera déplacé
         liste=plateau.deplacementPossible(tourJoueur,x,y)
@@ -98,11 +100,13 @@ def versus_cpu(blitz):
         while res==False:
             x, y = clic()
             res=plateau.deplacerAnneau(tourJoueur,liste,x,y,nouv_x,nouv_y)
+        afficher_plateau(plateau._plateau, tourJoueur, 2)
         #Regarder s'il y a un alignement
         tableau_alignement=plateau.alignement()
         #Si des alignements sont enregistrés
         if len(tableau_alignement)>0:
-            res=Grille.retirer_alignement(tableau_alignement, x, y, True)
+            res=plateau.retirer_alignement(tableau_alignement, x, y, True)
+            afficher_plateau(plateau._plateau, tourJoueur, 3)
             if res==1:
                 #prendre un input de joueur et si c'est l'un de ses anneaux le retirer:
                 x, y = clic()
@@ -113,7 +117,9 @@ def versus_cpu(blitz):
         tourJoueur=1 if tourJoueur==2 else 2
     return plateau.anneau_retiréJ1, plateau.anneau_retiréJ2 #si la condition de victoire est remplie, envoyer les resultat pour l'écran de fin
 
+
 if nombre_joueurs == 2:
     boucle_de_jeu(blitz)
 elif nombre_joueurs == 1:
     versus_cpu(blitz)
+
